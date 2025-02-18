7c25d97c70e29b48d5c9865cd8e78c1a68fb72cf
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-02-18 18:16:54 +0100

    fixed the home link path.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
         <section className="home">
             <h1>Connect, Chat, and Give Plastic a Second Life!</h1>
             <p>Join a community dedicated to recycling and sustainability. Chat with others, exchange plastics, and contribute to a greener future-one conversation at a time!</p>
-            <Link to='/login' className='home-button'>Find Nearby Plastics</Link>
+            <Link to='/signup' className='home-button'>Find Nearby Plastics</Link>
         </section>
     )
 }
