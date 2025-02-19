860650c91859a72d610a81ac539aa2f7643ff90c
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-02-19 18:44:43 +0100

    saved user only for first google login, kept changed city and phone safe.

--- src/utils/googleLogin.js
+++ src/utils/googleLogin.js
@@ -1,5 +1,5 @@
 import { signInWithPopup } from "firebase/auth";
-import { setDoc, doc } from "firebase/firestore";
+import { setDoc,getDoc, doc } from "firebase/firestore";
 import { auth, googleProvider, db } from "../config/config";
 import { getSignUpErrorMessage } from "./signupErrors";
 
@@ -7,14 +7,8 @@ export async function googleLogin(navigate) {
     try {
         const userData = await signInWithPopup(auth, googleProvider)
         const user = userData.user
-        
-        await setDoc(doc(db, "users", user.uid), {
-            firstName: user.displayName.split(' ')[0],
-            lastName: user.displayName.split(' ')[1],
-            email: user.email,
-            city: '',
-            phone: ''
-        })
+
+        await saveUserToFirebase(user)
 
         navigate('/profile')
         return null
@@ -22,3 +16,18 @@ export async function googleLogin(navigate) {
         return getSignUpErrorMessage(err.code)
     }
 }
+
+async function saveUserToFirebase(user) {
+    const userRef = doc(db, "users", user.uid);
+    const userSnap = await getDoc(userRef);
+
+    if (!userSnap.exists()) {
+        // User does not exist, so create a new document
+        await setDoc(userRef, {
+            firstName: user.displayName ? user.displayName.split(' ')[0] : '',
+            lastName: user.displayName ? user.displayName.split(' ')[1] : '',
+            email: user.email,
+            city: '',
+            phone: ''
+        });
+}}
