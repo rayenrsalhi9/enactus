6583581a75447b24816febe51d97bcc21034a007
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-02-20 19:45:13 +0100

    added animated icons to home page and not found section.

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,9 +1,16 @@
 import { Link } from 'react-router-dom'
+import Lottie from 'lottie-react'
+import animationData from '../../animations/lottieflow-404-12-3-000000-easey.json'
 import './NotFound.css'
 
 export default function NotFound() {
     return (
         <section className="not-found">
+            <Lottie 
+                animationData={animationData} 
+                loop={true}
+                style={{ width: 300, height: 300 }}
+            />
             <h1>Sorry, the page you were looking for was not found.</h1>
             <Link to='/'>Return to home page</Link>
         </section>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom'
+import Lottie from 'lottie-react'
+import animationData from '../../animations/arrow.json'
 import './Home.css'
 
 export default function Home() {
@@ -6,7 +8,14 @@ export default function Home() {
         <section className="home">
             <h1>Connect, Chat, and Give Plastic a Second Life!</h1>
             <p>Join a community dedicated to recycling and sustainability. Chat with others, exchange plastics, and contribute to a greener future-one conversation at a time!</p>
-            <Link to='/login' className='home-button'>Find Nearby Plastics</Link>
+            <Link to='/login' className='home-button'>
+                Find Nearby Plastics
+                <Lottie 
+                    className='arrow'
+                    animationData={animationData} 
+                    loop={true}
+                />
+            </Link>
         </section>
     )
 }
